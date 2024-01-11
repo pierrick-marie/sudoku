a99836be5b3aeaf8e7ac8b04f0bbc694bff0de36
Pierrick <mail@pierrickmarie.info>
2024-01-11 22:26:24 +0100

    Tests :/

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# sudoku
+Sudoku
+=======
+
 A NodeJs and Electron based sudoku solver
+
+# Sources
+
+* [React boiler plate](https://github.com/electron-react-boilerplate/electron-react-boilerplate)
+
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,17 +1,67 @@
-// import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { MemoryRouter as Router, Link, Routes, Route } from 'react-router-dom';
 // import icon from '../../assets/icon.svg';
 // import './App.css';
+
+function Home() { 
+	return (
+		<div>Accueil</div>
+	); 
+}
+
+function About() { 
+	return (
+		<div>À propos</div>
+	); 
+}
+
+function Contact() { 
+	return (
+		<div>Contact</div>
+	);
+}
 
 function Hello() {
 	return (
 		<div>
-			<h1>My Sudoku !</h1>
+			<div className="Hello">
+			</div>
+			<h1>electron-react-boilerplate</h1>
+			<div className="Hello">
+				<a
+					href="https://electron-react-boilerplate.js.org/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<button type="button">
+						<span role="img" aria-label="books">
+							📚
+						</span>
+						Read our docs
+					</button>
+				</a>
+				<a
+					href="https://github.com/sponsors/electron-react-boilerplate"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<button type="button">
+						<span role="img" aria-label="folded hands">
+							🙏
+						</span>
+						Donate
+					</button>
+				</a>
+			</div>
 		</div>
 	);
 }
 
 export default function App() {
 	return (
-		<Hello />
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</Router>
 	);
 }
