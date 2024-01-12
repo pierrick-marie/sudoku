0c9ffcbcac6c32c47217170581a3f67365b4bcf9
Pierrick <mail@pierrickmarie.info>
2024-01-12 13:44:52 +0000

    Test with tictactoe

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
+// import App from './App';
+
+import TicTacToe from './TicTacToe';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-root.render(<App />);
+// root.render(<App />);
+root.render(<TicTacToe />);
 
 // calling IPC exposed from preload script
 window.electron.ipcRenderer.once('ipc-example', (arg) => {
